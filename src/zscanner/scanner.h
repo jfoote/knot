2704112653e8b6728212ae15aac601496b8e4333
Libor Peltan <libor.peltan@nic.cz>
2017-12-01 19:06:15 +0100

    zscanner: support for loading zone from non-normal file (e.g. pipe)
    ...by reading it whole into dynamically growing memory buffer

--- src/zscanner/scanner.h
+++ src/zscanner/scanner.h
@@ -202,6 +202,8 @@ struct scanner {
 		const char *end;
 		/*! Indication for the final block parsing. */
 		bool eof;
+		/*! Indication of being mmap()-ed (malloc()-ed otherwise). */
+		bool mmaped;
 	} input;
 
 	/*! File input parameters. */
